8e40da8576479e1f6a535cae123f3d6a8b75ff00
Rosendo Martinez <rosendomartinez270@gmail.com>
2024-07-23 15:56:22 -0500

    Vec2 Header + Imp.

---
+++ src/Vec2.cpp
@@ -0,0 +1,101 @@
+#include <cmath>
+#include "Vec2.h"
+
+Vec2::Vec2(float x, float y)
+    : x(x)
+    , y(y)
+{
+}
+
+Vec2::Vec2()
+{
+}
+
+bool Vec2::operator == (const Vec2& rhs) const
+{
+    return x == rhs.x && y == rhs.y;
+}
+
+bool Vec2::operator != (const Vec2& rhs) const
+{
+    return x != rhs.x || y == rhs.y;
+}
+
+Vec2 Vec2::operator + (const Vec2& rhs) const
+{
+    return Vec2(x + rhs.x, y + rhs.y);
+}
+
+Vec2 Vec2::operator - (const Vec2& rhs) const
+{
+    return Vec2(x - rhs.x, y - rhs.y);
+}
+
+Vec2 Vec2::operator * (const Vec2& rhs) const
+{
+    return Vec2(x * rhs.x, y * rhs.y);
+}
+
+Vec2 Vec2::operator / (const Vec2& rhs) const
+{
+    return Vec2(x / rhs.x, y / rhs.y);
+}
+
+Vec2 Vec2::operator * (const float val) const
+{
+    return Vec2(x*val, y*val);
+}
+
+void Vec2::operator += (const Vec2& rhs)
+{
+    x += rhs.x;
+    y += rhs.y;
+}
+
+void Vec2::operator -= (const Vec2& rhs)
+{
+    x -= rhs.x;
+    y -= rhs.y;
+}
+
+void Vec2::operator *= (const float val)
+{
+    x *= val;
+    y *= val;
+}
+
+void Vec2::operator /= (const float val)
+{
+    x /= val;
+    y /= val;
+}
+
+void Vec2::normalize()
+{
+    float l = length();
+    x = x/l;
+    y = y/l;
+}
+
+float Vec2::length() const
+{
+    return std::sqrt(x*x + y*y);
+}
+
+float Vec2::dist(const Vec2& v) const
+{
+    return std::sqrt((x - v.x) * (x - v.x) + (y - v.y) * (y - v.y));
+}
+
+float Vec2::distSqr(const Vec2& v) const
+{
+    return (x - v.x) * (x - v.x) + (y - v.y) * (y - v.y);
+}
+
+void Vec2::polar(const float a, const float r)
+{
+    const float pi = 3.1415f;
+    const float rad = a * pi/180;
+    x = r * std::cos(rad);
+    y = r * std::sin(rad);
+}
---
+++ src/Vec2.h
@@ -0,0 +1,31 @@
+#pragma once
+
+class Vec2
+{
+public:
+    float x = 0;
+    float y = 0;
+
+    bool operator == (const Vec2& rhs) const;
+    bool operator != (const Vec2& rhs) const;
+
+    Vec2 operator +  (const Vec2& rhs) const;
+    Vec2 operator -  (const Vec2& rhs) const;
+    Vec2 operator *  (const Vec2& rhs) const;
+    Vec2 operator /  (const Vec2& rhs) const;
+    Vec2 operator * (const float val) const;
+
+    void operator += (const Vec2& rhs);
+    void operator -= (const Vec2& rhs);
+    void operator *= (const float val);
+    void operator /= (const float val);
+
+    Vec2();
+    Vec2(float x, float y);
+
+    void   normalize ();
+    float  length    () const;
+    float  dist      (const Vec2& v) const;
+    float  distSqr   (const Vec2& v) const;
+    void   polar     (const float a, const float r);
+};
