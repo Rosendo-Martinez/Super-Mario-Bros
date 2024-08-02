dc528215b847419971752b5e329f14dfcddbd8db
Rosendo Martinez <rosendomartinez270@gmail.com>
2024-08-01 19:05:23 -0500

    Window Opens

--- src/GameEngine.cpp
+++ src/GameEngine.cpp
@@ -1,11 +1,16 @@
 #include "GameEngine.h"
+#include "Scene_Play.h"
 
 GameEngine::GameEngine()
 {
+    init(""); 
 }
 
 void GameEngine::init(const std::string & assetSpecFilePath) // load in all assets, create window, frame limit, set menu scene
 {
+    m_window.create(sf::VideoMode(64*10,64*10), "Super Mario World");
+    m_sceneMap["Play"] = std::make_shared<Scene_Play>(this, assetSpecFilePath);
+    m_currentScene = "Play";
 }
 
 void GameEngine::update()
@@ -34,6 +39,15 @@ void GameEngine::quit() // closes the game
 
 void GameEngine::run() // main game loop
 {
+    while (m_window.isOpen())
+    {
+        sf::Event event;
+        while (m_window.pollEvent(event)) {}; // temporary so that OS doesn't btch
+
+        // TODO: passing ACTIONS to scene
+        m_sceneMap[m_currentScene]->update();
+        m_sceneMap[m_currentScene]->sRender();
+    }
 }
 
 sf::RenderWindow & GameEngine::window()
