5e37c0be32e51e4e72f40bb9c81c4b39ee87bb22
Rosendo Martinez <rosendomartinez270@gmail.com>
2024-08-03 16:45:14 -0500

    Assets: Refactor

--- src/Assets.cpp
+++ src/Assets.cpp
@@ -1,5 +1,6 @@
 #include "Assets.h"
 #include <iostream>
+#include <cassert>
 
 Assets::Assets()
 {
@@ -8,11 +9,8 @@ Assets::Assets()
 void Assets::addTexture(const std::string & name, const std::string & path)
 {
     sf::Texture texture;
-    if (!texture.loadFromFile(path))
-    {
-        std::cout << "Error: could not load " << name << " texture with path " << path << std::endl;
-        return;
-    }
+    bool result = texture.loadFromFile(path);
+    assert(result && "Failed to load texture");
     
     m_textures[name] = texture;
 }
@@ -22,52 +20,40 @@ void Assets::addAnimation(const std::string & name, const Animation & animation)
     m_animations[name] = animation;
 }
 
-void Assets::addSound(std::string name, std::string path)
+void Assets::addSound(const std::string & name, const std::string & path)
 {
 }
 
-void Assets::addFont(std::string name, std::string path)
+void Assets::addFont(const std::string & name, const std::string & path)
 {
     sf::Font font;
-    if (!font.loadFromFile(path))
-    {
-        std::cout << "Error: could not load " << name << " font with path " << path << std::endl;
-        return;
-    }
+    bool result = font.loadFromFile(path);
+    assert(result && "Failed to load font");
 
     m_fonts[name] = font;
 }
 
-sf::Texture & Assets::getTexture(const std::string & name)
+const sf::Texture & Assets::getTexture(const std::string & name) const
 {
-    if (m_textures.find(name) == m_textures.end())
-    {
-        std::cout << "Error: called getTexture() with name " << name <<  ", but texture does not exist!\n";
-    }
+    assert(m_textures.find(name) == m_textures.end() && "Key is wrong or texture does not exist.");
 
-    return m_textures[name];
+    return m_textures.at(name);
 }
 
-Animation & Assets::getAnimation(const std::string & name)
+const Animation & Assets::getAnimation(const std::string & name) const
 {
-    if (m_animations.find(name) == m_animations.end())
-    {
-        std::cout << "Error: called getAnimation() with name " << name << ", but animation does not exist!\n";
-    }
+    assert(m_animations.find(name) == m_animations.end() && "Key is wrong or animation does not exist.");
 
-    return m_animations[name];
+    return m_animations.at(name);
 }
 
-sf::Sound & Assets::getSound(std::string name)
+const sf::Sound & Assets::getSound(const std::string & name) const
 {
 }
 
 const sf::Font & Assets::getFont(const std::string & name) const
 {
-    if (m_fonts.find(name) == m_fonts.end())
-    {
-        std::cout << "Error: called getFont() with name " << name << ", but font does not exist!\n";
-    }
+    assert(m_animations.find(name) == m_animations.end() && "Key is wrong or font does not exist.");
 
     return m_fonts.at(name);
 }
--- src/Assets.h
+++ src/Assets.h
@@ -18,11 +18,11 @@ public:
 
     void addTexture(const std::string & name, const std::string & path);
     void addAnimation(const std::string & name, const Animation & animation);
-    void addSound(std::string name, std::string path);
-    void addFont(std::string name, std::string path);
+    void addSound(const std::string & name, const std::string & path);
+    void addFont(const std::string & name, const std::string & path);
 
-    sf::Texture & getTexture(const std::string & name);
-    Animation & getAnimation(const std::string & name);
-    sf::Sound & getSound(std::string name);
+    const sf::Texture & getTexture(const std::string & name) const;
+    const Animation & getAnimation(const std::string & name) const;
+    const sf::Sound & getSound(const std::string & name) const;
     const sf::Font & getFont(const std::string & name) const;
 };
