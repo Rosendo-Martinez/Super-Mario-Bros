77e994ac1a2e6026f75f05ab181bc81cfb14a087
Rosendo Martinez <rosendomartinez270@gmail.com>
2024-08-16 12:53:23 -0500

    Fix Animation class bug with scaling

--- src/Animation.cpp
+++ src/Animation.cpp
@@ -23,7 +23,7 @@ Animation::Animation(const std::string & name, const sf::Texture & t, size_t fra
     , m_speed(speed)
 {
     m_size = Vec2((float)t.getSize().x / frameCount, (float)t.getSize().y);
-    m_sprite.setOrigin(m_size.x / 2.0f, m_size.y / 2.0f);
+    m_sprite.setOrigin(m_size.x * scaleX / 2.0f, m_size.y * scaleY / 2.0f);
     m_sprite.setTextureRect(sf::IntRect(0, 0, m_size.x, m_size.y));
     m_sprite.setScale(sf::Vector2f(scaleX, scaleY));
 }
