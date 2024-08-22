78712653c72eb33a645abf6aea442a35cf9492f0
Rosendo Martinez <rosendomartinez270@gmail.com>
2024-08-22 14:23:01 -0500

    GetOverLap

--- src/Physics.cpp
+++ src/Physics.cpp
@@ -1,17 +1,32 @@
 #include "Physics.h"
 #include "Components.h"
+#include <cmath>
+
+Vec2 getOverLap(Vec2 aPos, Vec2 bPos, Vec2 aHalfSize, Vec2 bHalfSize)
+{
+    Vec2 delta(std::abs(aPos.x - bPos.x), std::abs(aPos.y - bPos.y));
+    float ox = aHalfSize.x + bHalfSize.x - delta.x;
+    float oy = aHalfSize.y + bHalfSize.y - delta.y;
+    
+    return Vec2(ox,oy);
+}
 
 Vec2 Physics::GetOverlap(std::shared_ptr<Entity> a, std::shared_ptr<Entity> b)
 {
-    // TODO: return the overlap rectangle size of the bounding boxes of entity a and b
-
-    return Vec2(0,0);
+    CTransform aCT = a->getComponent<CTransform>();
+    CTransform bCT = b->getComponent<CTransform>();
+    CBoundingBox aCB = a->getComponent<CBoundingBox>();
+    CBoundingBox bCB = b->getComponent<CBoundingBox>();
+    
+    return getOverLap(aCT.pos, bCT.pos, aCB.halfSize, bCB.halfSize);
 }
 
 Vec2 Physics::GetPreviousOverlap(std::shared_ptr<Entity> a, std::shared_ptr<Entity> b)
 {
-    // TODO: return the previous overlap rectangle size of the bounding boxes of entity a and b
-    //       previous overlap uses the entity's previous position
+    CTransform aCT = a->getComponent<CTransform>();
+    CTransform bCT = b->getComponent<CTransform>();
+    CBoundingBox aCB = a->getComponent<CBoundingBox>();
+    CBoundingBox bCB = b->getComponent<CBoundingBox>();
 
-    return Vec2(0,0);
+    return getOverLap(aCT.prevPos, bCT.prevPos, aCB.halfSize, bCB.halfSize);
 }
